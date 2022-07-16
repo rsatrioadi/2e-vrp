ae476554d3b320a56121a7c0fa165a6093c23480
daspers <buddy90_lost@yahoo.co.id>
2022-07-17 02:31:38 +0800

    separate ServiceConfig from ServicesConfig

--- src/nl/tue/vrp/config/ServicesConfig.java
+++ src/nl/tue/vrp/config/ServicesConfig.java
@@ -3,47 +3,6 @@ package nl.tue.vrp.config;
 import java.util.List;
 
 public class ServicesConfig {
-    static public class ServiceConfig {
-        protected int customerID;
-        protected int depotID;
-        protected int demand;
-        protected int serviceTime;
-
-        public ServiceConfig() {
-        }
-
-        public int getCustomerID() {
-            return customerID;
-        }
-
-        public void setCustomerID(int customerID) {
-            this.customerID = customerID;
-        }
-
-        public int getDepotID() {
-            return depotID;
-        }
-
-        public void setDepotID(int depotID) {
-            this.depotID = depotID;
-        }
-
-        public int getDemand() {
-            return demand;
-        }
-
-        public void setDemand(int demand) {
-            this.demand = demand;
-        }
-
-        public int getServiceTime() {
-            return serviceTime;
-        }
-
-        public void setServiceTime(int serviceTime) {
-            this.serviceTime = serviceTime;
-        }
-    }
 
     List<ServiceConfig> deliveries;
     List<ServiceConfig> pickups;
